0c31ab78bfa4c0051f3166779439b8c0f7eee89b
unknown <bahaaahmed19902012@gmail.com>
2024-01-13 20:29:18 +0200

    first commit

---
+++ front/assets/js/main.js
@@ -0,0 +1,54 @@
+console.log("cloud");
+const wave = document.querySelector("#wave");
+const lex = document.querySelector("#lex");
+const skyline = document.querySelector("#skyline");
+const heyazah = document.querySelector("#heyazah");
+const businessYard = document.querySelector("#business-yard");
+
+wave.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("wdem");
+  location.pathname = location.pathname.concat("the-wave");
+});
+
+lex.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("wdem1");
+  location.pathname = location.pathname.concat("lex");
+});
+
+skyline.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("wdem2");
+  location.pathname = location.pathname.concat("sky-line");
+});
+
+heyazah.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("wdem3");
+  location.pathname = location.pathname.concat("heyazah");
+});
+
+businessYard.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("wdem4");
+  location.pathname = location.pathname.concat("business-yard");
+});
+function draw() {
+  console.log("drw");
+  const ctx = document.getElementById("canvas").getContext("2d");
+  const img = new Image();
+  img.onload = () => {
+    console.log("first");
+    ctx.drawImage(img, 0, 0, 10, 100);
+    ctx.drawImage(img, 5, 0, 10, 100);
+    ctx.drawImage(img, 0, 10, 10, 100);
+    console.log(img);
+  };
+  img.src = "/assets/images/clouds.png";
+
+  ctx.translate(5);
+
+  window.requestAnimationFrame(draw);
+}
+draw();
---
+++ server.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+console.log(__dirname);
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// serve frontend
+app.use(express.static(path.join(__dirname, "front")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "front", "index.html"));
+});
+
+app.get("/heyazah", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "front", "heyazah.html"));
+});
+
+app.get("/lex", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "front", "lex.html"));
+});
+
+app.get("/the-wave", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "front", "wave.html"));
+});
+
+app.get("/business-yard", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "front", "yard.html"));
+});
+
+app.get("/sky-line", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "front", "skyline.html"));
+});
+// app.use(notFound);
+// app.use(errHandler);
+
+app.listen(5533, () => console.log(`server is running`));
